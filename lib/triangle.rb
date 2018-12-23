33eb37da2126ecf890e2e918e272114accaf89f5
github username <github email address>
2018-12-23 19:06:34 +0000

    learn submit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+# lib/triangle.rb
+
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :l, :w, :h
+
+  def initialize(l,w,h)
+    @l = l
+    @w = w
+    @h = h
+  end
+
+  def kind
+    if (@l + @w <= @h) || (@l + @h <= @w) || (@w + @h <= @l) || (@l == 0 || @w == 0|| @h == 0)
+      raise TriangleError
+
+    else
+      if @l == @w && @w == @h
+        :equilateral
+      elsif (@l == @w || @l == @h || @w == @h)
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
