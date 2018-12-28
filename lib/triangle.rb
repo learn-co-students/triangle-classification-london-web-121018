eb0e125214d569c1dbb9f0fc7574aa788e3df380
github username <github email address>
2018-12-28 22:43:38 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+
+require 'pry'
+
 class Triangle
-  # write code here
+
+  class TriangleError < StandardError
+  end
+  
+  attr_accessor :l, :w, :h
+
+  @@all = []
+
+  def initialize(l, w, h)
+    @l = l
+    @w = w
+    @h = h 
+    @@all << self
+  end
+
+  def kind
+    side_array = []
+    side_array = [@l,@w,@h]
+    sorted_array = side_array.sort
+    if @l <= 0 || @w <=0 || @h <= 0  || (sorted_array[0] + sorted_array[1]) <= sorted_array[2]
+      raise TriangleError
+    
+    elsif sorted_array.uniq.length == 1
+      return :equilateral
+
+    elsif sorted_array.uniq.length == 2
+      return :isosceles
+    
+    elsif sorted_array.uniq.length == 3
+      return :scalene
+    end
+  end
+
+  def self.all 
+    @@all
+  end
+
 end
+
